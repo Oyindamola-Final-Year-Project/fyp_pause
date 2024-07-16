8bb7538333c0405f3ee41307f9d7bce3979fea2f
Oyindamola J. Lewu <oyindamola.lewu@pau.edu.ng>
2024-07-16 21:30:18 +0100

    one more ui fix

--- app/apply/formapi/route.ts
+++ app/apply/formapi/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: NextRequest) {
     console.log('Data inserted successfully:');
     return NextResponse.json({ success: true, data: result.rows[0] }, { status: 200 });
   } catch (error) {
-    console.error('Error inserting data:', error);
+    // console.error('Error inserting data:', error);
     return NextResponse.json({ success: false, error: 'Internal Server Error' }, { status: 500 });
   }
 }
